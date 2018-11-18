77aee019374e21c46322d6be08087c876da73742
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2018-11-18 15:02:13 +0100

    Added download functionality

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 var app = new Vue({
     el: "#app",
     data: {
+        version: "",
+        isPreRelease: false,
         downloads: {
             windows: "#",
             macos: "#",
@@ -9,6 +11,26 @@ var app = new Vue({
     }
 });
 
-// $.getJSON('https://api.github.com/repos/deltaproject/Delta/releases/latest', function(data) {
-//     console.log(data);
-// });
+$.getJSON("https://api.github.com/repos/deltaproject/Delta/releases", function(data) {
+    app.version = data[0].tag_name;
+    app.isPreRelease = data[0].prerelease;
+    
+    var assets = data[0].assets;
+    for (let i = 0; i < assets.length; i++) {
+        const element = assets[i];
+        let name = element.name;
+        let url = element.browser_download_url;
+
+        if (name.indexOf("windows") != -1) {
+            app.downloads.windows = url;
+        }
+
+        else if (name.indexOf("macOS") != -1) {
+            app.downloads.macos = url;
+        }
+
+        else if (name.indexOf("linux") != -1) {
+            app.downloads.linux = url;
+        }
+    }
+});
